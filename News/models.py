8082ce5a487f5f25deab3b91e6fd1baac8e809a8
FIlIN <filin122@list.ru>
2023-02-01 00:45:12 +0400

    initial version

---
+++ News/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+
+
+class Author(models.Model):
+    rating_auth = models.IntegerField(default=0)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Is it need NULL = True??
+
+    def update_rating(self):
+        postRat = self.post_set.all().aggregate(postRating=Sum('rating_post'))
+        pRat = 0
+        pRat += postRat.get('postRating')
+
+        commentRat = self.user.comment_set.all().aggregate(commentRating=Sum('rating_com'))
+        cRat = 0
+        cRat += commentRat.get('commentRating')
+
+        self.rating_auth = pRat * 3 + cRat
+        self.save()
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=64, unique=True)
+
+
+# article = 'AR'
+# new = 'NW'
+
+
+class Post(models.Model):
+    article = 'AR'
+    new = 'NW'
+    TYPES = [
+        (article, 'Статья'),
+        (new, 'Новость')
+    ]
+    type = models.CharField(max_length=2, choices=TYPES, default=article)
+    time_post = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=122)
+    text_post = models.TextField()
+    rating_post = models.IntegerField(default=0)
+
+    authors = models.ForeignKey(Author, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, through='PostCategory')
+
+    def like(self):
+        self.rating_post += 1
+        self.save()
+
+    def dislike(self):
+        self.rating_post -= 1
+        self.save()
+
+    def preview(self):
+        return self.text_post[0:123] + '...'
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    text_com = models.TextField()
+    time_com = models.DateTimeField(auto_now_add=True)
+    rating_com = models.IntegerField(default=0)
+
+    posts = models.ForeignKey(Post, on_delete=models.CASCADE)
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def like(self):
+        self.rating_com += 1
+        self.save()
+
+    def dislike(self):
+        self.rating_com -= 1
+        self.save()
+# Create your models here.
